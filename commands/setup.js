b375332a3cfa8f8dfe72da9158d46bb49982f9d2
BilloXD <142104317+BilloXD@users.noreply.github.com>
2024-04-18 15:10:29 +0500

    Update setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -128,7 +128,7 @@ module.exports = {
         authorIcon,
       } = messageData;
 
-      const welcomeMessage = `**Welcome** **${member}** **to** **${member.guild.name}**`;
+      const welcomeMessage = `<a:welcome:1230458323237867530> **Welcome** **${member}** **to** **${member.guild.name}**`;
       const welcomeMessageDm = `<a:taj:859462735691382784>  **This message has been sent from** **${member.guild.name} by** **<@725620582942507065>**`;
 
       const embed = new EmbedBuilder()
